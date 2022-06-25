695a990b2a7ee5a3078a200019c53fdb3bd98ce5
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-06-25 00:55:27 -0400

    added const's and a let

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+const startButton = document.getElementById("start-btn")
+const nextButton = document.getElementById("next-btn")
+const questionContainer = document.getElementById("container")
+const questionEl = document.getElementById("question")
+const ansButtonEl = document.getElementById("choices")
+
+let shuffledQuestions, currentQuestionIndex
