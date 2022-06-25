56427a4ee745647f08d5ce11fe9eddb0df948e40
Sufyaan Vaidya <vaidyasufyaan@gmail.com>
2022-06-25 00:56:30 -0400

    added a function to start the game

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,14 @@ const questionEl = document.getElementById("question")
 const ansButtonEl = document.getElementById("choices")
 
 let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener("click", startGame)
+
+function startGame() {
+console.log("started")
+startButton.classList.add("hide")
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
+questionContainer.classList.remove("hide")
+setNextQuestion()
+}
